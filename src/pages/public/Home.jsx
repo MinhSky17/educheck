03f2cf645242561edb01a2a9311cb17474c213ac
DESKTOP-SO9R61K\ADMIN <nguyenthiminh.sky.17@gmail.com>
2026-06-19 11:08:59 +0700

    header

---
+++ src/pages/public/Home.jsx
@@ -0,0 +1,12 @@
+import Header from '../../components/public/Header';
+
+
+function Home(){
+    return(
+        <div className="font-sans text-gray-800 antialiased bg-white">
+            <Header/>
+        </div>
+    );
+}
+
+export default Home;
